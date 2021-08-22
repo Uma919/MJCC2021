050c9665ba132e1e4474b64b03eaab8b7612e423
yuinopi <63541124+yuinopi@users.noreply.github.com>
2021-08-22 19:15:00 +0900

    ソースコード追加

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # MJCC2021
 本リポジトリは[M5Stack Japan Creativity Contest 2021](https://info.switch-science.com/m5stack-creativity-contest-2021)開発リポジトリです．  
-ダストセンサにより喫煙を感知し、喫煙者に禁煙を促します．
+．
 
 # 必要なモノ
 ## 制御基板や電子部品
- * [ATOM Lite](https://ssci.to/6262)
- * [GROVE - ダストセンサ](https://ssci.to/3081)
  * [ATOM NS4168搭載 スピーカーキット](https://ssci.to/7092)
- * [ショートワイヤ付きミニ楕円形スピーカー（8Ω 1W）](https://ssci.to/5804)
+ * [GROVE - ダストセンサ](https://ssci.to/3081)
+
---
+++ src/main/main.ino
@@ -0,0 +1,88 @@
+#include "M5Atom.h"
+#include "AtomSPK.h"
+#include "Ticker.h"
+
+ATOMSPK atomSPK;
+Ticker ticker;
+
+/*--- Sensor ---*/
+int pin = 32;
+unsigned long duration;
+unsigned long starttime;
+unsigned long sampletime_ms = 30000;  // sampele 30s ;
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+float concentration = 0;
+
+/*--- Ticker ---*/
+const float period = 60 * 15;  // Speak once per 15 minutes
+
+/*--- Interruption Processing ---*/
+void speak(){
+    if(concentration >= 3000){  // Warning
+        for(int n = 0; n < 5; n ++){
+            atomSPK.playBeep(2000, 200, 10000, false);
+            atomSPK.playBeep(1000, 200, 10000, false);
+        }
+    }else if(3000 > concentration && concentration > 1000){
+        for(int n = 0; n < 3; n ++){  // Caution
+            atomSPK.playBeep(2000, 200, 10000, false);
+            atomSPK.playBeep(1000, 200, 10000, false);
+        }
+    }else if(1000 >= concentration){  // Notice
+        for(int n = 0; n < 1; n ++){
+            atomSPK.playBeep(2000, 200, 10000, false);
+            atomSPK.playBeep(1000, 200, 10000, false);
+        }
+    }
+}
+
+/*--- Main Program ---*/
+void setup(){
+    /* ATOM Lite */
+    M5.begin(true, false, true);
+
+    /* Debug */
+    Serial.begin(9600);
+
+    /* Sensor */
+    pinMode(pin, INPUT);
+    starttime = millis();  // get the current time;
+
+    /* Speaker */
+    atomSPK.begin();
+
+    /* Ticker */
+    ticker.attach(period, speak);
+}
+
+void loop(){
+    /* Sensing */
+    duration = pulseIn(pin, LOW);
+    lowpulseoccupancy = lowpulseoccupancy + duration;
+
+    if((millis() - starttime) > sampletime_ms){  //if the sampel time == 30s
+        ratio = lowpulseoccupancy / (sampletime_ms * 10.0);  // Integer percentage 0=>100
+        concentration = 1.1 * pow(ratio, 3) - 3.8 * pow(ratio, 2) + 520 * ratio + 0.62;  // using spec sheet curve
+        
+        /* Debug */
+        //Serial.print(lowpulseoccupancy);
+        //Serial.print(",");
+        //Serial.print(ratio);
+        //Serial.print(",");
+        Serial.println(concentration);
+        
+        /* Display */
+        if(concentration >= 3000){
+            M5.dis.drawpix(0, 0x00ff00);  // Red
+        }else if(3000 > concentration && concentration > 1000){
+            M5.dis.drawpix(0, 0xffff00);  // Yellow
+        }else if(1000 >= concentration){
+            M5.dis.drawpix(0, 0xff0000);  // Green
+        }
+
+        lowpulseoccupancy = 0;
+        starttime = millis();
+    }
+}
+
